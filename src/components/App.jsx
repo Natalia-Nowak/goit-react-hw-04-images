05d7ae4e85359dc68fa5c700771b733bf3704602
Natalia-Nowak <nowak.natalia.080@gmail.com>
2023-11-18 14:15:00 +0100

    fixed deployment

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -31,6 +31,7 @@ export const App = () => {
 
   useEffect(() => {
     fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const handleSubmit = async event => {
